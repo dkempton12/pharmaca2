cf73fbefd01100b9fecdcf814088f52d50ead37a
Edward Sztukowski <edsztukowski@gmail.com>
2018-05-08 08:47:40 -0600

    added sidebar.css that should go on any pages that have a sidebar with content

--- Site Components/Instagram/insta.js
+++ Site Components/Instagram/insta.js
@@ -30,6 +30,7 @@ requirejs(['jquery'], function( $ ) {
             $('#insta-feed').owlCarousel({
                 loop:true,
                 margin:10,
+                nav: true,
                 responsive:{
                     0:{
                         items:1
