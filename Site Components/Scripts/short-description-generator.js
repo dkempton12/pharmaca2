5953c6d2d70b6a1e462b2d64f7c03c993de1a934
Edward Sztukowski <edsztukowski@gmail.com>
2018-06-12 09:30:28 -0600

    Allowed short description generator to use render HTML instead of plain text

--- Site Components/Scripts/short-description-generator.js
+++ Site Components/Scripts/short-description-generator.js
@@ -1,3 +1,4 @@
+//create the short description field that accepts the content we pass in
 function createShortDescriptionField(content) {
     var shortDescription = 
         '<div class="product attribute overview">'
@@ -6,18 +7,24 @@ function createShortDescriptionField(content) {
             + '</div>'
         +'</div>';
     
-    var whereToPlace = jQuery('.product-info-price')[0]
-    jQuery(whereToPlace.after(decodeURI(shortDescription)))
+    //find the area to place the short description fields
+    var whereToPlace = jQuery('.product-info-price')[0];
+    //insert the short description field into the page
+    jQuery(shortDescription).insertAfter(whereToPlace)
     
 }
     
 function getShortDescriptionContent() {
-    var longDescription = document.querySelectorAll('.product.info.detailed .description .value')[0].innerText
-    var shortDescription = longDescription.slice(0, 120);
-    shortDescription += '...'
-
+    //get the long description
+    var longDescription = document.querySelectorAll('.product.info.detailed .description .value')[0].innerHTML
+    //create the short description from the long description
+    var shortDescription = longDescription.slice(0, 150);
+    shortDescription += '...<a href="#tab-label-product.info.description">Read More</a>';
+    //Call the create function with the short description passed in as 'content'
     createShortDescriptionField(shortDescription)
 }
-    
-    
-getShortDescriptionContent()
+
+//check if the short description exists and run the function if it does    
+if (jQuery(".product.attribute.overview").length <= 0) {
+    getShortDescriptionContent()
+}
