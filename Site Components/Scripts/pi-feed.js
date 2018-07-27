e520a68cf1bb5a61dd53a9fd46a0ec4a200272c5
Edward Sztukowski <edsztukowski@gmail.com>
2018-07-27 13:21:03 -0600

    refactors of cat pages, refactor pi, refactor mega menu, added newsletter signup code

--- Site Components/Scripts/pi-feed.js
+++ Site Components/Scripts/pi-feed.js
@@ -1,41 +1,49 @@
-updatePiLayout = () => {
-    const igoBody = document.getElementsByClassName('igo_boxbody');
-    const igoBox = document.querySelectorAll('.igo_product');
-    let altText = '';
-    let price = '';
-    let sale = '';
+require([
+    'jquery'
+], function ($) {
+    'use strict';
+
+    var scriptUrl = "https://85667.recs.igodigital.com/a/v2/85667/home/recommend.js";
+
+    var updatePiLayout = function () {
+    let igoBody = document.getElementsByClassName('igo_boxbody');
+    let igoBox = document.querySelectorAll('.igo_product');
 
     //add row to main body of feed
     igoBody[0].className += " row";
 
     //add bootstrap classes to each product box
-    igoBox.forEach((box) => {
+    igoBox.forEach(function (box) {
         let altText = box.getElementsByClassName('igo_product_product_name_value')[0].innerText;
         let price = box.getElementsByClassName('igo_product_regular_price_value')[0].innerText;
         let sale = box.getElementsByClassName('igo_product_sale_price_value')[0].innerText;
-        let shopButton = box.getElementsByTagName('a')[1];
+        let links = box.querySelectorAll("a");
+        
+        links.forEach((link) => {
+            if (link.textContent) {
+                let shopButton = link;
+                shopButton.className += "action primary";
+                shopButton.innerText = 'SHOP';
+                $(link).closest('.igo_product').append(shopButton);
+            }
+        });
 
         //add bootstrap classes
         box.className += " col-md-3 center";
         box.getElementsByTagName('img')[0].alt = altText;
 
         //add strikethroughs
-        if (price > sale) {
-           box.getElementsByClassName('igo_product_regular_price_value')[0].className += ' strikethrough';
+        if (price > sale && sale !== '$0.00') {
+            box.getElementsByClassName('igo_product_regular_price_value')[0].className += ' strikethrough';
         }
 
         //Check to make sure sale pricing isn't the same as full price
-        if(sale == '$0.00' || price == sale) {
+        if (sale === '$0.00' || price === sale) {
             box.getElementsByClassName('igo_product_sale_price')[0].remove();
-            
+
         }
+    });
+};
 
-        //add Shop Button
-        shopButton.className += "action primary";
-        shopButton.innerText = 'SHOP';
-
-    })
-
-}
-
-updatePiLayout();
+    $.getScript(scriptUrl, updatePiLayout);
+});
